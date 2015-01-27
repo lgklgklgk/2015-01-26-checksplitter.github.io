23574c1ea6a3e17c803164a9ff338510e763c7bb
Luke Korth <lukekorth@gmail.com>
2015-01-27 10:08:30 -0600

    defined instance variables using methods

--- checksplitter.rb
+++ checksplitter.rb
@@ -1,16 +1,30 @@
 require "pry"
 class Checksplitter
   def initialize(param1, param2, param3)
-    @meal = param1
-    @group = param2
-    @tip = param3
+    @meal = meal
+    @group = group
+    @tip = tip
   end
 end
 
-def get_tip
-  (@meal * @tip) / 100
+def meal
+  @meal
 end
+
+def group
+  @group
+end
+
+def tip
+  @tip
+end
+
+def get_tip
+  (meal * tip) / 100
+end
+
 def check_split
-  (@meal + ((@meal * @tip) / 100)) / @group
+  (meal + ((meal * tip) / 100)) / group
 end
 binding.pry
+
