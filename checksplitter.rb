8e453860d2b81eb759183ceaa2826b08516e6eea
Luke Korth <lukekorth@gmail.com>
2015-01-27 12:50:46 -0600

    i am trying to add the diner club option not sure if this will work

--- checksplitter.rb
+++ checksplitter.rb
@@ -5,26 +5,44 @@ class Checksplitter
     @group = group
     @tip = tip
   end
-
   def meal
     @meal
   end
-
   def group
     @group
   end
-
   def tip
     @tip
   end
-
   def get_tip
     (meal * tip) / 100
   end
-
   def check_split
     (meal + ((meal * tip) / 100)) / group
   end
 end
+
+class Dinerclub
+  def initialize(outing)
+    @outing = outing
+    @checks = []
+    add_check("Beginning Balance", 0)
+  end
+  def new_check(name, amount)
+    add_check(name, amount)
+  end
+  def add_check(name, amount)
+    @checks.push(Checksplitter.new(meal, group, tip))
+  end
+  def balance
+    balance = 0.0
+    @checks.each do |x|
+      balance += x.check_split
+    end
+    return balance
+  end
+end
+    
+  
 binding.pry
 
