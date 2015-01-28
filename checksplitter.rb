71184fe0c3126fff20bbcef51cfab0d1a5fc6447
Luke Korth <lukekorth@gmail.com>
2015-01-27 20:59:38 -0600

    ok now im using instance variables for what was throwing me and i think it's going to work.

--- checksplitter.rb
+++ checksplitter.rb
@@ -12,30 +12,19 @@ class Checksplitter
   def check_split
     (meal + ((meal * tip) / 100)) / group
   end
-  def get_split
-    self.check_split
-  end
 end
 
 class Dinerclub
+  attr_reader :split
   def initialize(outing)
     @outing = outing
     @checks = []
   end
   def split_check(m, g, t)
-    split_check = Checksplitter.new(m, g, t).check_split
+    @split = Checksplitter.new(m, g, t).check_split
+  
   end
-  def add_check(name, amount)
-    @checks.push(name: name, amount: amount)
-  end
-  def balance
-    balance = 0.0
-    @checks.each do |x|
-      puts x
-    end
-    return balance
-  end
-end
+end   
     
   
 binding.pry
